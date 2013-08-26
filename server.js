65963d73a662ea7f77c34406349d521643108452
Salem <salemhilal@gmail.com>
2013-08-26 11:21:11 -0400

    Added cacheing. Because why not.

--- server.js
+++ server.js
@@ -7,51 +7,74 @@ var https = require('https')
 
 var url = "https://clusters.andrew.cmu.edu/printerstats/"
 var cache = {};
+var cache_ttl = 30*1000; // 30 seconds
 
 // Utility function that downloads a URL and invokes
 // callback with the data.
 function download(url, callback) {
-  https.get(url, function(res) {
-    var data = "";
-    res.on('data', function (chunk) {
-      data += chunk;
+  if(cache[url] 
+        && cache[url].timestamp 
+        && (Date.now() - cache[url].timestamp) <= cache_ttl) {
+    console.log("Cache hit");
+    callback(cache[url].body);
+  }
+
+  else {
+    https.get(url, function(res) {
+      var data = "";
+      res.on('data', function (chunk) {
+        data += chunk;
+      });
+      res.on("end", function() {
+
+        // We have the data, stick it in the cache and run callback.
+        console.log("Populating cache");
+        cache[url] = {
+          timestamp : Date.now(),
+          body : data
+        }
+        callback(data);
+
+      });
+    }).on("error", function() {
+      callback(null);
     });
-    res.on("end", function() {
-      callback(data);
-    });
-  }).on("error", function() {
-    callback(null);
-  });
+  }
+
 }
 
+// Returns a JSON representation of a given table row. 
+//    @$   - Cheerio selector
+//    @row - the row to parse.
 function toJson($, row) {
   var t = {};
-  t.name = $($(row).find("td")[0]).html();
-  t.icon = $($(row).find("td")[1]).find("img").attr("src");
-  t.message = $($(row).find("td")[2]).html();
-  t.status = $($(row).find("td")[3]).html();
-  t.trays = $($(row).find("td")[4]).find("font").map(function(i,e){
+  t.name        = $($(row).find("td")[0]).html();
+  t.icon        = $($(row).find("td")[1]).find("img").attr("src");
+  t.message     = $($(row).find("td")[2]).html();
+  t.status      = $($(row).find("td")[3]).html();
+  t.timestring  = $($(row).find("td")[5]).html();
+  t.trays       = $($(row).find("td")[4]).find("font").map(function(i,e){
     return $(e).html();
   });
-  t.timestring = $($(row).find("td")[5]).html();
+
   return t;
 }
 
 function parse(data) {
-    if (data) {
-      // Load data
-      var $ = cheerio.load(data);
+  if (data) {
+    // Load data
+    var $ = cheerio.load(data);
 
-      // Find all the rows with data in them
-      var t = $(".epi-rowEven, .epi-rowOdd").map(function(i,e){
-        // Extract data from rows.
-        return toJson($, e);
-      });
-      return t;
-    }
-    else console.log("error");  
-    return null;
+    // Find all the rows with data in them
+    var t = $(".epi-rowEven, .epi-rowOdd").map(function(i,e){
+      // Extract data from rows.
+      return toJson($, e);
+    });
+    return t;
   }
+  else console.log("error");  
+  return null;
+}
 
 // Test
 app.get('/printers', function(req, res){
@@ -65,7 +88,6 @@ app.get('/printers', function(req, res){
   });
 
 });
-
 
 
 app.listen(3000);
